68bde70b12236d43c198ab5e7667164fc8165308
Yahya Gilany <gilanyym@ucmail.uc.edu>
2020-05-28 15:05:18 -0400

    modified folder structure tests

--- tests/0.folderStructure.test.js
+++ tests/0.folderStructure.test.js
@@ -4,50 +4,45 @@ const _ = require(`lodash`);
 describe(`folder structure`, function () {
   test(`root directory is properly setup`, () => {
     const rootTree = dirTree(`${__dirname}/..`);
-    const nodes = rootTree.children.map(node => node.name);
+    const nodes = rootTree.children.map((node) => node.name);
 
-    expect(nodes).toContain(`resources`);
-    expect(nodes).toContain(`index.html`);
     const expectedNodes = [
       `.git`,
       `.github`,
       `.vscode`,
+      `node_modules`,
       `resources`,
       `tests`,
-      `.gitignore`,
       `.babelrc`,
       `.eslintrc.json`,
       `.gitignore`,
+      `.prettierignore`,
       `.prettierrc`,
       `jest.config.js`,
       `index.html`,
       `package.json`,
-      `README.md`
+      `README.md`,
     ];
     expect(nodes).toEqual(expect.arrayContaining(expectedNodes));
   });
 
   test(`resources directory is properly setup`, () => {
     const resourcesTree = dirTree(`${__dirname}/../resources`);
-    const nodes = resourcesTree.children.map(node => node.name);
-    const expectedNodes = [
-      `images`,
-      `scripts`,
-      `styles`
-    ];
+    const nodes = resourcesTree.children.map((node) => node.name);
+    const expectedNodes = [ `vendor`, `css`, `js` ];
     expect(nodes).toEqual(expect.arrayContaining(expectedNodes));
   });
 
   test(`scripts directory includes one index.js files`, () => {
-    const scriptsTree = dirTree(`${__dirname}/../resources/scripts`);
-    const nodes = scriptsTree.children.map(node => node.name);
+    const scriptsTree = dirTree(`${__dirname}/../resources/js`);
+    const nodes = scriptsTree.children.map((node) => node.name);
 
     expect(nodes).toContain(`index.js`);
   });
 
   test(`styles directory includes one styles.css files`, () => {
-    const stylesTree = dirTree(`${__dirname}/../resources/styles`);
-    const nodes = stylesTree.children.map(node => node.name);
+    const stylesTree = dirTree(`${__dirname}/../resources/css`);
+    const nodes = stylesTree.children.map((node) => node.name);
 
     expect(nodes).toContain(`styles.css`);
   });
